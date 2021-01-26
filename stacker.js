75f7360cca93aac09f1d081abe5debe6b170ab74
Ethan May <ethaniel.may@gmail.com>
2021-01-26 16:54:36 -0700

    Player Name Area Fully Styled

--- stacker.js
+++ stacker.js
@@ -5,7 +5,8 @@ function setPlayer(event) {
     console.log(form.playerName.value + " is now the current Player's Name")
     let playerName = form.playerName.value
 
-    document.getElementById("nameSpace").classList.remove("hidden")
+    document.getElementById("nameArea").classList.remove("hidden")
+    document.getElementById("changePlayerButton").classList.remove("hidden")
     document.getElementById("nameSpace").innerHTML = playerName
     form.reset()
     form.classList.add("hidden")
